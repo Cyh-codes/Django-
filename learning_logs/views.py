bd5d7383b7168ad7e344a859c9f7a0f6916768c7
Cyh-codes <cyh.codes@gmail.com>
2026-07-08 21:20:00 +0800

    完成index.HTML初步建立,学习了创建网页简单,完成作业Pizza

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+def index(request):
+    # 学习笔记的主页
+    return render(request, 'learning_logs/index.html')
+
